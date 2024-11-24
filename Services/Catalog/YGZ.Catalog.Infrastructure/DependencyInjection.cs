2502b8385109a6ac69e6609327ece7c7e3b16cbd
bachlex1608 <lxbachit03.working@gmail.com>
2024-11-25 01:32:40 +0700

    wip

--- Services/Catalog/YGZ.Catalog.Infrastructure/DependencyInjection.cs
+++ Services/Catalog/YGZ.Catalog.Infrastructure/DependencyInjection.cs
@@ -3,6 +3,7 @@ using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using System.Reflection;
 using YGZ.Catalog.Application.Core.Abstractions.EventBus;
 using YGZ.Catalog.Application.Core.Abstractions.Uploading;
 using YGZ.Catalog.Domain.Core.Abstractions.Common;
@@ -14,36 +15,58 @@ namespace YGZ.Catalog.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
     {
         services.Configure<CloudinarySettings>(configuration.GetSection(CloudinarySettings.SettingKey));
         services.Configure<MessageBrokerSettings>(configuration.GetSection(MessageBrokerSettings.SettingKey));
 
         services.AddScoped<IUploadService, UploadService>();
 
-        services.AddMassTransit(busConfiguarator =>
+        services.AddMassTransit(config =>
         {
-            busConfiguarator.SetKebabCaseEndpointNameFormatter();
+            config.SetKebabCaseEndpointNameFormatter();
 
-            //busConfiguarator.AddConsumer<ProductCreatedEventComsumer>();
-
-            busConfiguarator.UsingRabbitMq((context, configurator) =>
+            if (assembly != null)
             {
-                MessageBrokerSettings settings = context.GetRequiredService<IOptions<MessageBrokerSettings>>().Value;
+                config.AddConsumers(assembly);
+            }
 
-                Console.WriteLine("settings.Host" + settings.Host);
-                Console.WriteLine("settings.Username" + settings.Username);
-                Console.WriteLine("settings.Password" + settings.Password);
+            Console.WriteLine("MessageBrokerSettings:Host: " + configuration["MessageBrokerSettings:Host"]);
+            Console.WriteLine("MessageBrokerSettings:UserName: " + configuration["MessageBrokerSettings:UserName"]);
+            Console.WriteLine("MessageBrokerSettings:Password: " + configuration["MessageBrokerSettings:Password"]);
 
-                configurator.Host(new Uri(settings.Host), host =>
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBrokerSettings:Host"]!), host =>
                 {
-                    host.Username(settings.Username);
-                    host.Password(settings.Password);
+                    host.Username(configuration["MessageBrokerSettings:UserName"]!);
+                    host.Password(configuration["MessageBrokerSettings:Password"]!);
                 });
 
                 configurator.ConfigureEndpoints(context);
-
             });
+            //busConfiguarator.SetKebabCaseEndpointNameFormatter();
+
+            ////busConfiguarator.AddConsumer<ProductCreatedEventComsumer>();
+
+            //busConfiguarator.UsingRabbitMq((context, configurator) =>
+            //{
+            //    MessageBrokerSettings settings = context.GetRequiredService<IOptions<MessageBrokerSettings>>().Value;
+
+            //    Console.WriteLine("settings.Host" + settings.Host);
+            //    Console.WriteLine("settings.Username" + settings.Username);
+            //    Console.WriteLine("settings.Password" + settings.Password);
+
+            //    configurator.Host(new Uri(settings.Host), host =>
+            //    {
+            //        host.Username(settings.Username);
+            //        host.Password(settings.Password);
+            //    });
+
+            //    configurator.ConfigureEndpoints(context);
+
+            //});
         });
 
         services.AddTransient<IEventBus, EventBus>();
