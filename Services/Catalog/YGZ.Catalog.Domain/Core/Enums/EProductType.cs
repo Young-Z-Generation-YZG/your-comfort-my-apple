d8963ebfd9a74dcf9c4b8df02060eceb8988f65f
Your Name <your.email@example.com>
2025-09-20 10:41:31 +0700

    refactor: change State to EState | ProductType to EProductType

--- Services/Catalog/YGZ.Catalog.Domain/Core/Enums/EProductType.cs
+++ Services/Catalog/YGZ.Catalog.Domain/Core/Enums/EProductType.cs
@@ -8,10 +8,10 @@ public class EProductType : SmartEnum<EProductType>
     public EProductType(string name, int value) : base(name, value) { }
 
     public static readonly EProductType UNKNOWN = new(nameof(UNKNOWN), 0);
-    public static readonly EProductType IPHONE = new(nameof(IPHONE), 1);
-    public static readonly EProductType IPAD = new(nameof(IPAD), 2);
-    public static readonly EProductType MACBOOK = new(nameof(MACBOOK), 3);
-    public static readonly EProductType WATCH = new(nameof(WATCH), 4);
-    public static readonly EProductType HEADPHONE = new(nameof(HEADPHONE), 5);
-    public static readonly EProductType ACCESSORY = new(nameof(ACCESSORY), 6);
+    public static readonly EProductType IPHONE = new(nameof(IPHONE), 0);
+    public static readonly EProductType IPAD = new(nameof(IPAD), 0);
+    public static readonly EProductType MACBOOK = new(nameof(MACBOOK), 0);
+    public static readonly EProductType WATCH = new(nameof(WATCH), 0);
+    public static readonly EProductType HEADPHONE = new(nameof(HEADPHONE), 0);
+    public static readonly EProductType ACCESSORY = new(nameof(ACCESSORY), 0);
 }
