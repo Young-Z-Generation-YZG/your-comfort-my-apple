4485b9f027fe3ef885f478c392a06641b7309bbf
Your Name <your.email@example.com>
2025-09-20 16:32:47 +0700

    [discount] update: configure database for Event, EventProductSKU

---
+++ Services/Discount/YGZ.Discount.Infrastructure/Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YGZ.Discount.Domain.Core.Enums;
+using YGZ.Discount.Domain.Event;
+using YGZ.Discount.Domain.Event.ValueObjects;
+
+namespace YGZ.Discount.Infrastructure.Persistence.Configurations;
+
+public class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+    public void Configure(EntityTypeBuilder<Event> builder)
+    {
+        builder.ToTable("Events");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).ValueGeneratedNever().HasConversion(
+            id => id.Value,
+            dbid => EventId.Of(dbid)
+        );
+
+        builder.Property(x => x.State) // Updated from Status to State
+               .HasConversion(
+                   x => x.Name,
+                   x => EState.FromName(x, false)
+               )
+               .HasColumnName("State");
+    }
+}
---
+++ Services/Discount/YGZ.Discount.Infrastructure/Persistence/Configurations/EventProductSKUConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YGZ.Discount.Domain.Core.Enums;
+using YGZ.Discount.Domain.Event;
+using YGZ.Discount.Domain.Event.Entities;
+using YGZ.Discount.Domain.Event.ValueObjects;
+
+namespace YGZ.Discount.Infrastructure.Persistence.Configurations;
+
+public class EventProductSKUConfiguration : IEntityTypeConfiguration<EventProductSKU>
+{
+    public void Configure(EntityTypeBuilder<EventProductSKU> builder)
+    {
+        builder.ToTable("EventProductSKUs");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).ValueGeneratedNever().HasConversion(
+            id => id.Value,
+            dbid => EventProducSKUId.Of(dbid)
+        );
+
+        // foreign key configuration
+        builder.HasOne<Event>().WithMany().HasForeignKey(x => x.EventId).IsRequired();
+
+        // other property configurations
+        builder.Property(x => x.ProductType)
+               .HasConversion(
+                   x => x.Name,
+                   x => EProductType.FromName(x, false)
+               )
+               .HasColumnName("ProductType");
+
+        builder.Property(x => x.DiscountType)
+                .HasConversion(
+                     x => x.Name,
+                     x => EDiscountType.FromName(x, false)
+                )
+                .HasColumnName("DiscountType");
+    }
+}
