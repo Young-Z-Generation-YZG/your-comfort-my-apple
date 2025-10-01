e20cb21791e6e7c84963633fe84d0ae00ba887bb
thongqkit <thongqkit@gmail.com>
2025-10-02 00:20:28 +0700

    chore: move svg to icon component

--- apps/client/src/components/icon/index.ts
+++ apps/client/src/components/icon/index.ts
@@ -1,1 +1,3 @@
 export { default as AppleIcon } from './apple-icon';
+export { default as ApplePickupIcon } from './apple-pickup-icon';
+export { default as DeliveryTruckIcon } from './delivery-truck-icon';
