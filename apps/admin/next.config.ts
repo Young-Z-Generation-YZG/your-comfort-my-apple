86051f0556873d467c30f213698c336f1c723370
bachlex03 <lxbachit03.working@gmail.com>
2025-10-26 20:59:36 +0700

    init apps/admin

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   transpilePackages: ["@kltn-monorepo/ui-shadcn", "@kltn-monorepo/utils"],
 };
 
