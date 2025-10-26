307526793f1910c355394eda63b5521b226972da
bachlex03 <lxbachit03.working@gmail.com>
2025-10-26 21:41:59 +0700

    wip

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@kltn-monorepo/ui-shadcn/components/Button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>TEST 2</h1>
+    <div className="bg-black">
+      <h1 className="text-blue-400">TEST 2</h1>
+      <Button variant="outline">Test BTN</Button>
     </div>
   );
 }
