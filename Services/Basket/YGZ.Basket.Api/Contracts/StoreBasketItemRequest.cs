eb6cf1fb0129fa71f4f9322130e49e9de8564c74
bachlex03 <lxbachit03.working@gmail.com>
2025-12-29 10:47:25 +0700

    docs: delete some thing

--- Services/Basket/YGZ.Basket.Api/Contracts/StoreBasketItemRequest.cs
+++ Services/Basket/YGZ.Basket.Api/Contracts/StoreBasketItemRequest.cs
@@ -6,7 +6,6 @@ namespace YGZ.Basket.Api.Contracts;
 public sealed record StoreBasketItemRequest
 {
     [Required]
-    [JsonPropertyName("sku_id")]
     public required string SkuId { get; init; }
 
     [Required]
