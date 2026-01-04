e799077da27c4f684ba273644e3ef4234ad1562e
bachlex03 <lxbachit03.working@gmail.com>
2026-01-04 17:27:24 +0700

    fix: gRPC call with self SSL for gRPC

--- Services/Discount/YGZ.Discount.Grpc/Program.cs
+++ Services/Discount/YGZ.Discount.Grpc/Program.cs
@@ -1,10 +1,45 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using YGZ.Discount.Application;
 using YGZ.Discount.Grpc;
 using YGZ.Discount.Grpc.GrpcServices;
 using YGZ.Discount.Infrastructure;
 using YGZ.Discount.Infrastructure.Persistence.Extensions;
 
+// Helper method to create self-signed certificate for Docker
+static X509Certificate2 CreateSelfSignedCertificate()
+{
+    var subjectName = "CN=localhost";
+    using var rsa = RSA.Create(2048);
+    var request = new CertificateRequest(subjectName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+    request.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.KeyEncipherment, false));
+    request.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension([new Oid("1.3.6.1.5.5.7.3.1")], false));
+    
+    var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow.AddDays(-1), DateTimeOffset.UtcNow.AddYears(1));
+    return new X509Certificate2(certificate.Export(X509ContentType.Pfx, "password"), "password", X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet);
+}
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Kestrel for HTTP and HTTPS
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // HTTP endpoint with HTTP/2 support for gRPC
+    options.ListenAnyIP(8080, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+    });
+    
+    // HTTPS endpoint with HTTP/2
+    options.ListenAnyIP(8081, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+        // Generate self-signed certificate for Docker
+        var cert = CreateSelfSignedCertificate();
+        listenOptions.UseHttps(cert);
+    });
+});
 var services = builder.Services;
 
 // Add services to the container.
--- Services/Ordering/YGZ.Ordering.Api/Program.cs
+++ Services/Ordering/YGZ.Ordering.Api/Program.cs
@@ -1,33 +1,51 @@
 using HealthChecks.UI.Client;
 using Keycloak.AuthServices.Authorization;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using YGZ.BuildingBlocks.Shared.Extensions;
 using YGZ.Ordering.Api;
 using YGZ.Ordering.Api.Extensions;
 using YGZ.Ordering.Application;
 using YGZ.Ordering.Infrastructure;
 using YGZ.Ordering.Infrastructure.Persistence.Extensions;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+// Helper method to create self-signed certificate for Docker
+static X509Certificate2 CreateSelfSignedCertificate()
+{
+    var subjectName = "CN=localhost";
+    using var rsa = RSA.Create(2048);
+    var request = new CertificateRequest(subjectName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+    request.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.KeyEncipherment, false));
+    request.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension([new Oid("1.3.6.1.5.5.7.3.1")], false));
+    
+    var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow.AddDays(-1), DateTimeOffset.UtcNow.AddYears(1));
+    return new X509Certificate2(certificate.Export(X509ContentType.Pfx, "password"), "password", X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet);
+}
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Kestrel for HTTP and HTTPS
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // HTTP endpoint with HTTP/2 support for gRPC
+    options.ListenAnyIP(8080, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+    });
+    
+    // HTTPS endpoint with HTTP/2
+    options.ListenAnyIP(8081, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+        // Generate self-signed certificate for Docker
+        var cert = CreateSelfSignedCertificate();
+        listenOptions.UseHttps(cert);
+    });
+});
 var services = builder.Services;
 var host = builder.Host;
-
-// Configure Kestrel to accept HTTP/2 over plain HTTP for gRPC
-// builder.WebHost.ConfigureKestrel(options =>
-// {
-//     // Configure all endpoints to support both HTTP/1.1 and HTTP/2
-//     options.ConfigureEndpointDefaults(listenOptions =>
-//     {
-//         listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-//     });
-    
-//     // Explicitly configure the HTTP endpoint to support HTTP/2
-//     options.ListenAnyIP(8080, listenOptions =>
-//     {
-//         listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-//     });
-// });
 
 // Add Layers
 services
